2f55a347fd3fd6505364444513ab2f77d3672b1f
xvWind <26201669+xvWind@users.noreply.github.com>
2026-09-17 22:42:54 -0400

    improve rate further

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -12,9 +12,9 @@ def train_model(X, y, seed=seed):
     model = GradientBoostingClassifier(
         learning_rate=0.01,
         n_estimators=100,
-        max_depth=3,
+        max_depth=4,
         subsample=1,
-        min_samples_leaf=10,
+        min_samples_leaf=15,
         random_state=seed
     )
     model.fit(X, y)
