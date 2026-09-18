5c2c60d8bcc597893612d146412a42773927e022
xvWind <26201669+xvWind@users.noreply.github.com>
2026-09-17 22:57:42 -0400

    futher improve misclassification rate

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -10,11 +10,11 @@ def train_model(X, y, seed=seed):
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
-        learning_rate=0.01,
+        learning_rate=0.05,
         n_estimators=100,
-        max_depth=4,
-        subsample=1,
-        min_samples_leaf=15,
+        max_depth=3,
+        subsample=0.8,
+        min_samples_leaf=10,
         random_state=seed
     )
     model.fit(X, y)
